057c1c3cea1877a0886c02c56e43fc1d27dcb202
SprayHank <fantasyoui@gmail.com>
2013-08-12 11:57:29 +0800

    保存

---
+++ config/config_global_default.php
@@ -0,0 +1,9 @@
+<?PHP
+
+$CONFIG = array();
+
+$CONFIG['db']['host'] = '';//Database host
+$CONFIG['db']['name'] = '';//Database name
+$CONFIG['db']['user'] = '';//Database user
+$CONFIG['db']['pass'] = '';//Database password
+$CONFIG['db']['pre_'] = '';//Database prefix
